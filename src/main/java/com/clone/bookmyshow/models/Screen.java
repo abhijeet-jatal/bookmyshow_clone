2da6c357eab82a8c1c2d7f1bf293e22fb9798c6e
abhijeet-jatal <abhijeetjatal@gmail.com>
2024-02-20 01:40:52 +0530

    added screen model

---
+++ src/main/java/com/clone/bookmyshow/models/Screen.java
@@ -0,0 +1,18 @@
+package com.clone.bookmyshow.models;
+
+import com.clone.bookmyshow.enums.Feature;
+import jakarta.persistence.*;
+import lombok.Data;
+import java.util.List;
+
+@Data
+@Entity
+public class Screen extends BaseModel{
+    private String name;
+    @OneToMany
+    private List<Seat> seats;
+    @Enumerated(EnumType.ORDINAL)
+    @ElementCollection
+    private List<Feature> features;
+    private Theater theater;
+}
