441c8f13b8c69546d1b6363b369daa821fd53a2e
abhijeet-jatal <abhijeetjatal@gmail.com>
2024-02-20 01:36:23 +0530

    updated class name as per conviction

--- src/main/java/com/clone/bookmyshow/BookMyShowApplication.java
+++ src/main/java/com/clone/bookmyshow/BookMyShowApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BookmyshowApplication {
+public class BookMyShowApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BookmyshowApplication.class, args);
+		SpringApplication.run(BookMyShowApplication.class, args);
+
 	}
 
 }
