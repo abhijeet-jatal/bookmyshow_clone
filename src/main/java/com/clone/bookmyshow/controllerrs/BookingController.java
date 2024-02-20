cce5314084d30205c2882cdf651357b895a1d831
abhijeet-jatal <abhijeetjatal@gmail.com>
2024-02-20 16:04:21 +0530

    added booking controller

---
+++ src/main/java/com/clone/bookmyshow/controllerrs/BookingController.java
@@ -0,0 +1,10 @@
+package com.clone.bookmyshow.controllerrs;
+
+import com.clone.bookmyshow.dtos.BookMovieRequestDTO;
+import com.clone.bookmyshow.dtos.BookMovieResponseDTO;
+
+public class BookingController {
+    public BookMovieResponseDTO bookTicket(BookMovieRequestDTO bookMovieRequestDTO) {
+        return null;
+    }
+}
