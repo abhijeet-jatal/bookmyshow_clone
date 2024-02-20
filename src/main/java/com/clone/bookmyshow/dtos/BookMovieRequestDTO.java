20848002402ff4f9d9e40d86e509ec8d4cd74d7f
abhijeet-jatal <abhijeetjatal@gmail.com>
2024-02-20 16:04:41 +0530

    added booking request dto

---
+++ src/main/java/com/clone/bookmyshow/dtos/BookMovieRequestDTO.java
@@ -0,0 +1,10 @@
+package com.clone.bookmyshow.dtos;
+
+import java.util.List;
+
+public class BookMovieRequestDTO {
+    private List<Long> showSeatIds;
+    private Long showId;
+    private Long userId;
+
+}
