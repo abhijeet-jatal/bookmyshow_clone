ec012e0205183e8ceb749301e2140b0a750f4fa0
abhijeet-jatal <abhijeetjatal@gmail.com>
2024-02-20 16:04:47 +0530

    added booking response dto

---
+++ src/main/java/com/clone/bookmyshow/dtos/BookMovieResponseDTO.java
@@ -0,0 +1,9 @@
+package com.clone.bookmyshow.dtos;
+
+import com.clone.bookmyshow.enums.ResponseStatus;
+
+public class BookMovieResponseDTO {
+    private int amount;
+    private Long bookingId;
+    private ResponseStatus responseStatus;
+}
