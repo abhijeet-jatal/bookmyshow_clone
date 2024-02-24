edd76b31bb6c61435912a87f48cec43e92cd1525
abhijeet-jatal <abhijeetjatal@gmail.com>
2024-02-24 20:27:45 +0530

    chore: implemented show seat type repository and findAll

---
+++ src/main/java/com/clone/bookmyshow/repositories/ShowSeatTypeRepository.java
@@ -0,0 +1,14 @@
+package com.clone.bookmyshow.repositories;
+
+import com.clone.bookmyshow.models.Show;
+import com.clone.bookmyshow.models.ShowSeatType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ShowSeatTypeRepository extends JpaRepository<ShowSeatType, Long> {
+    List<ShowSeatType> findAllByShow(Show show);
+    
+}
