f888d9782603edef1d1785b9e3740dea66ed6dd7
abhijeet-jatal <abhijeetjatal@gmail.com>
2024-02-24 20:30:02 +0530

    chore: added getter and setter

--- src/main/java/com/clone/bookmyshow/dtos/BookMovieRequestDTO.java
+++ src/main/java/com/clone/bookmyshow/dtos/BookMovieRequestDTO.java
@@ -1,7 +1,10 @@
 package com.clone.bookmyshow.dtos;
 
+import lombok.Data;
+
 import java.util.List;
 
+@Data
 public class BookMovieRequestDTO {
     private List<Long> showSeatIds;
     private Long showId;
